16157a05209e7b2c9331d0532436b84d2c58a633
Diego Lorden <djlorden@gmail.com>
2014-10-27 14:03:00 -0700

    Update setup config

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
-import os
+from setuptools import find_packages
 from setuptools import setup
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-migrations-plus',
-    version='0.1',
-    packages=['migrations_plus'],
-    include_package_data=True,
-    description='Extra helpers for using django.db.migrations',
+    packages=find_packages(),
+    pbr=True,
+    setup_requires=['pbr'],
 )
